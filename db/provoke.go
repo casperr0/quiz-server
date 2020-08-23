c8e769aced2a8a295a2b0da1fdd66f03d2c31028
RainrainWu <s0958334772@gmail.com>
2020-08-23 22:26:14 +0800

    feat(db): finish operating interface of provoke table

---
+++ db/provoke.go
@@ -0,0 +1,30 @@
+package db
+
+// CreateProvoke create a new provoke with correct and message.
+func CreateProvoke(correct bool, message string) error {
+
+	createSQL := `
+	INSERT INTO provoke (correct, message)
+	SELECT $1::BOOLEAN, $2::VARCHAR
+	`
+	tx := database.MustBegin()
+	tx.MustExec(createSQL, correct, message)
+	tx.Commit()
+	return nil
+}
+
+// QueryProvokes query all provokes with the correctness.
+func QueryProvokes(correct bool) ([]Provoke, error) {
+
+	querySQL := `
+	SELECT p.id, p.correct, p.message
+	FROM provoke p
+	WHERE p.correct = $1
+	`
+	var provokes []Provoke
+	err := database.Select(&provokes, querySQL, correct)
+	if err != nil {
+		return nil, err
+	}
+	return provokes, nil
+}
